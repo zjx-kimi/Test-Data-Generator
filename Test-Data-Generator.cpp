123f46d203d40074415b33dc0a90cf0ea9315d4c
zjx-kimi <134782384+zjx-kimi@users.noreply.github.com>
2023-08-07 13:08:20 +0800

    Update Test-Data-Generator.cpp

--- Test-Data-Generator.cpp
+++ Test-Data-Generator.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <Windows.h>
 using namespace std;
+const int Mod = 1e9 + 7;
 void sleep (int x = 100) {
 	for (int i = 1; i <= 3; i++) {
 		Sleep (x);
@@ -8,7 +9,7 @@ void sleep (int x = 100) {
 	}
 	return;
 }
-int n, c, t;
+int n, m[1000000], c, t;
 void slow() {
 	cout << "请确保 g++.exe 已添加到环境变量\n";
 	cout << "请确保已将标程写入 std.cpp 中\n";
@@ -31,18 +32,24 @@ void slow() {
 	system ("g++.exe gen.cpp -o gen.exe");
 	system ("cls");
 	/*-----------------------------------*/
-	cout << "请输入数据组数:";
+	cout << "请输入Subtask组数:";
 	cin >> n;
+	cout << "请输入每组Subtask的数据数,以空格隔开:";
+	for (int i = 1; i <= n; i++) cin >> m[i];
 	system ("cls");
 	for (int i = 1; i <= n; i++) {
-		cout << "正在生成 Data" << i << ".in/out ";
-		sleep();
-		char Path_in[1000], Path_out[1000];
-		sprintf (Path_in, "gen.exe >Data%d.in", i);
-		sprintf (Path_out, "std.exe <Data%d.in >Data%d.out", i, i);
-		system (Path_in);
-		system (Path_out);
-		system ("cls");
+		for (int j = 1; j <= m[i]; j++) {
+			cout << "正在生成 Data" << i << "-" << j << ".in/out ";
+			sleep (100);
+			char Path_in[1000], Path_out[1000], Rand[100];
+			sprintf(Rand, "echo %d %d > rand.txt", i, j);
+			sprintf (Path_in, "gen.exe <rand.txt >Data%d-%d.in", i, j);
+			sprintf (Path_out, "std.exe <Data%d-%d.in >Data%d-%d.out", i, j, i, j);
+			system (Rand);
+			system (Path_in);
+			system (Path_out);
+			system ("cls");
+		}
 	}
 }
 void fast() {
@@ -67,18 +74,24 @@ void fast() {
 	system ("g++.exe gen.cpp -o gen.exe");
 	system ("cls");
 	/*-----------------------------------*/
-	cout << "请输入数据组数:";
+	cout << "请输入Subtask组数:";
 	cin >> n;
+	cout << "请输入每组Subtask的数据数,以空格隔开:";
+	for (int i = 1; i <= n; i++) cin >> m[i];
 	system ("cls");
 	for (int i = 1; i <= n; i++) {
-		cout << "正在生成 Data" << i << ".in/out ";
-		sleep (10);
-		char Path_in[1000], Path_out[1000];
-		sprintf (Path_in, "gen.exe >Data%d.in", i);
-		sprintf (Path_out, "std.exe <Data%d.in >Data%d.out", i, i);
-		system (Path_in);
-		system (Path_out);
-		system ("cls");
+		for (int j = 1; j <= m[i]; j++) {
+			cout << "正在生成 Data" << i << "-" << j << ".in/out ";
+			sleep (10);
+			char Path_in[1000], Path_out[1000], Rand[100];
+			sprintf(Rand, "echo %d %d > rand.txt", i, j);
+			sprintf (Path_in, "gen.exe <rand.txt >Data%d-%d.in", i, j);
+			sprintf (Path_out, "std.exe <Data%d-%d.in >Data%d-%d.out", i, j, i, j);
+			system (Rand);
+			system (Path_in);
+			system (Path_out);
+			system ("cls");
+		}
 	}
 }
 void user (int x) {
@@ -103,21 +116,28 @@ void user (int x) {
 	system ("g++.exe gen.cpp -o gen.exe");
 	system ("cls");
 	/*-----------------------------------*/
-	cout << "请输入数据组数:";
+	cout << "请输入Subtask组数:";
 	cin >> n;
+	cout << "请输入每组Subtask的数据数,以空格隔开:";
+	for (int i = 1; i <= n; i++) cin >> m[i];
 	system ("cls");
 	for (int i = 1; i <= n; i++) {
-		cout << "正在生成 Data" << i << ".in/out ";
-		sleep (x);
-		char Path_in[1000], Path_out[1000];
-		sprintf (Path_in, "gen.exe >Data%d.in", i);
-		sprintf (Path_out, "std.exe <Data%d.in >Data%d.out", i, i);
-		system (Path_in);
-		system (Path_out);
-		system ("cls");
+		for (int j = 1; j <= m[i]; j++) {
+			cout << "正在生成 Data" << i << "-" << j << ".in/out ";
+			sleep (x);
+			char Path_in[1000], Path_out[1000], Rand[100];
+			sprintf(Rand, "echo %d %d > rand.txt", i, j);
+			sprintf (Path_in, "gen.exe <rand.txt >Data%d-%d.in", i, j);
+			sprintf (Path_out, "std.exe <Data%d-%d.in >Data%d-%d.out", i, j, i, j);
+			system (Rand);
+			system (Path_in);
+			system (Path_out);
+			system ("cls");
+		}
 	}
 }
 int main() {
+	srand(time(0) + rand());
 	cout << "请输入你要使用的方案\n";
 	cout << "1. 慢速模式、质量好\n";
 	cout << "2. 快速模式、质量差\n";
@@ -128,7 +148,7 @@ int main() {
 	} else if (n == 2) {
 		fast();
 	} else {
-		cout << "输入的数为 x,那么使用时间为 x ×测试点数，x 越大质量越好。\n请输入x:";
+		cout << "输入的数为 x,那么使用时间为 3x × 测试点数，x 越大质量越好。\n请输入x:";
 		int x;
 		cin >> x;
 		user (x);
