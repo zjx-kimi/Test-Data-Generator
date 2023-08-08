f1ace8b52a4d230070d08f9594385d4f2b266f92
zjx-kimi <134782384+zjx-kimi@users.noreply.github.com>
2023-08-08 09:28:01 +0800

    Update Test-Data-Generator.cpp

--- Test-Data-Generator.cpp
+++ Test-Data-Generator.cpp
@@ -10,90 +10,6 @@ void sleep (int x = 100) {
 	return;
 }
 int n, m[1000000], c, t;
-void slow() {
-	cout << "请确保 g++.exe 已添加到环境变量\n";
-	cout << "请确保已将标程写入 std.cpp 中\n";
-	cout << "请确保已将数据生成器写入 gen.cpp 中\n";
-	system ("pause");
-	system ("cls");
-	/*-----------------------------------*/
-	cout << "正在删除文件";
-	sleep();
-	system ("del std.exe & del gen.exe");
-	system ("cls");
-	/*-----------------------------------*/
-	cout << "正在编译标程代码";
-	sleep();
-	system ("g++.exe std.cpp -o std.exe");
-	system ("cls");
-	/*-----------------------------------*/
-	cout << "正在编译数据生成器";
-	sleep();
-	system ("g++.exe gen.cpp -o gen.exe");
-	system ("cls");
-	/*-----------------------------------*/
-	cout << "请输入Subtask组数:";
-	cin >> n;
-	cout << "请输入每组Subtask的数据数,以空格隔开:";
-	for (int i = 1; i <= n; i++) cin >> m[i];
-	system ("cls");
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m[i]; j++) {
-			cout << "正在生成 Data" << i << "-" << j << ".in/out ";
-			sleep (100);
-			char Path_in[1000], Path_out[1000], Rand[100];
-			sprintf(Rand, "echo %d %d > rand.txt", i, j);
-			sprintf (Path_in, "gen.exe <rand.txt >Data%d-%d.in", i, j);
-			sprintf (Path_out, "std.exe <Data%d-%d.in >Data%d-%d.out", i, j, i, j);
-			system (Rand);
-			system (Path_in);
-			system (Path_out);
-			system ("cls");
-		}
-	}
-}
-void fast() {
-	cout << "请确保 g++.exe 已添加到环境变量\n";
-	cout << "请确保已将标程写入 std.cpp 中\n";
-	cout << "请确保已将数据生成器写入 gen.cpp 中\n";
-	system ("pause");
-	system ("cls");
-	/*-----------------------------------*/
-	cout << "正在删除文件";
-	sleep (10);
-	system ("del std.exe & del gen.exe");
-	system ("cls");
-	/*-----------------------------------*/
-	cout << "正在编译标程代码";
-	sleep (10);
-	system ("g++.exe std.cpp -o std.exe");
-	system ("cls");
-	/*-----------------------------------*/
-	cout << "正在编译数据生成器";
-	sleep (10);
-	system ("g++.exe gen.cpp -o gen.exe");
-	system ("cls");
-	/*-----------------------------------*/
-	cout << "请输入Subtask组数:";
-	cin >> n;
-	cout << "请输入每组Subtask的数据数,以空格隔开:";
-	for (int i = 1; i <= n; i++) cin >> m[i];
-	system ("cls");
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m[i]; j++) {
-			cout << "正在生成 Data" << i << "-" << j << ".in/out ";
-			sleep (10);
-			char Path_in[1000], Path_out[1000], Rand[100];
-			sprintf(Rand, "echo %d %d > rand.txt", i, j);
-			sprintf (Path_in, "gen.exe <rand.txt >Data%d-%d.in", i, j);
-			sprintf (Path_out, "std.exe <Data%d-%d.in >Data%d-%d.out", i, j, i, j);
-			system (Rand);
-			system (Path_in);
-			system (Path_out);
-			system ("cls");
-		}
-	}
-}
 void user (int x) {
 	cout << "请确保 g++.exe 已添加到环境变量\n";
 	cout << "请确保已将标程写入 std.cpp 中\n";
@@ -144,9 +60,9 @@ int main() {
 	cout << "3. 自定义模式\n";
 	cin >> n;
 	if (n == 1) {
-		slow();
+		user(100);
 	} else if (n == 2) {
-		fast();
+		user(10);
 	} else {
 		cout << "输入的数为 x,那么使用时间为 3x × 测试点数，x 越大质量越好。\n请输入x:";
 		int x;
